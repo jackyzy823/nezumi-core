f72f828ddbd898b4bd6097756c243f4e5145d2be
jackyzy823 <jackyzy823@gmail.com>
2015-03-24 22:54:03 +0800

    regular commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var info = require('./package');
 process.title = info.name;
 var util = require('util');
 var path = require('path');
+var events = require('events');
+
 
 process.on('SIGINT', function() {
   console.log(util.format('\rOoops! %s terminated.', info.name));
@@ -12,41 +14,69 @@ process.on('SIGINT', function() {
 });
 
 var usage = require('./lib/usage.js');
-var urls = usage.argv._;
-/* options do not need _(urls)*/
+var srcUrls = usage.argv._;
+/* options do not need _(srcUrls)*/
 var options = usage.argv;
 delete options._;
 
 // var options = argv;
 // delete options._;
 var matchExtractor = require('./lib/matcher.js').match;
-
 var extractorFolder = path.join(__dirname, 'lib', 'extractors');
 
-if (urls.length === 0 || options.help) {
+if (srcUrls.length === 0 || options.help) {
   usage.showHelp();
 }
+
+var multi = require('multimeter')(process);
 
 if (options.daemon) {
   /* Enter daemon */
   console.log('current not support');
   process.exit(1);
-} else {
-  urls.forEach(function(url, index) {
-    var url = url.toString().toLowerCase();
-    /* not a strict regexp*/
-    if (!url.match(/^https?:\/\/.+$/)) {
-      url = 'http://' + url;
+}
+srcUrls.forEach(function(srcUrl, index) {
+  var srcUrl = srcUrl.toString().toLowerCase();
+  /* not a strict regexp*/
+  if (!srcUrl.match(/^https?:\/\/.+$/)) {
+    srcUrl = 'http://' + srcUrl;
+  }
+  matchExtractor(srcUrl, function download(resModule) {
+    if (!resModule) {
+      console.log(util.format('unsupported url :%s', srcUrl));
+      return;
     }
-    matchExtractor(url, function download(resModule) {
-      if (!resModule) {
-        console.log(util.format('unsupported url :%s', url));
+    var p = require(path.join(extractorFolder, resModule))(srcUrl, options, function realDownload(err, infos) {
+      if (err) {
+        console.log(err);
         return;
       }
-      require(path.join(extractorFolder, resModule)).download(url, options, function(err, result) {
+      // infos  should be array even array.length==1-> 
+      //{ ,items:[{url:[string,string,...],title,size,},{url:[string],title,size},{url:function_to_gen_url,title,size},{url:[function_to_gen,function],title,size}]}
+      info.items.forEach(function(item, index) {
+        if (Array.isArray(item)) {
+          if (url)
 
+        }
+        if (typeof item === 'function') {
+          for (var len = item.length(), i = 0; i < len; i++) {
+            var url = item.next();
+            download_url(url, )
+          }
+        } else if (Array.isArray(item)) {
+          item.forEach(function(url, idx) {
+            if (typeof url === 'function') {
+              url = url();
+            }
+
+          });
+        }
       });
-    });
 
+    });
+    p.on('display', function(msg) {
+      // do sth with msg
+    })
   });
-}
+
+});
