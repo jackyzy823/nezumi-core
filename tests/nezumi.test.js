e36bfb05df55df8e4e775640f989c2deb0b43961
jackyzy823 <jackyzy823@gmail.com>
2015-05-20 23:59:19 +0800

    fix test

--- tests/nezumi.test.js
+++ tests/nezumi.test.js
@@ -2,7 +2,7 @@
 var match = require('../lib/matcher.js').match;
 var assert = require('assert');
 match('http://www.iqiyi.com/yinyue/20130716/302007c528607f58.html', function(result) {
-  assert.equal('iqiyi.js', result);
+  assert.equal('iqiyi', result);
 });
 
 match('http://v.pptv.com/show/xt7FQ6sRgb8iaoAg.html', function(result) {
