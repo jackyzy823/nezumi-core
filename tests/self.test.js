c91ac7e0ae8838099fcd9b6f44cbf90f65be13a5
jackyzy823 <jackyzy823@gmail.com>
2015-08-09 21:39:47 +0800

    iqiyi new key and use mocha for test

--- tests/self.test.js
+++ tests/self.test.js
@@ -1,21 +1,37 @@
 var nezumi = require('../index');
 
-nezumi.extract('http://v.pptv.com/show/5DdW1DyiaElCzMZk.html', function(err, urls, options) {
-  console.log(err, urls, options);
-});
+describe('test availabale', function() {
 
-nezumi.extract('http://www.iqiyi.com/v_19rrnscu0w.html', function(err, urls, options) {
-  console.log(err, urls, options);
-});
+  it('pptv', function(done) {
 
-nezumi.extract('http://www.letv.com/ptv/vplay/20036169.html', function(err, urls, options) {
-  console.log(err, urls, options);
-});
+    nezumi.extract('http://v.pptv.com/show/5DdW1DyiaElCzMZk.html', function(err, urls, options) {
+      console.log(err, urls, options);
+      done();
+    });
+  });
 
-nezumi.extract('http://tv.sohu.com/20140318/n396785017.shtml', function(err, urls, options) {
-  console.log(err, urls, options);
-});
-
-nezumi.extract('http://my.tv.sohu.com/pl/8990047/80342721.shtml', function(err, urls, options) {
-  console.log(err, urls, options);
+  it('iqiyi', function(done) {
+    nezumi.extract('http://www.iqiyi.com/v_19rrnscu0w.html', function(err, urls, options) {
+      console.log(err, urls, options);
+      done();
+    });
+  });
+  it('letv', function(done) {
+    nezumi.extract('http://www.letv.com/ptv/vplay/20036169.html', function(err, urls, options) {
+      console.log(err, urls, options);
+      done();
+    });
+  });
+  it('sohu', function(done) {
+    nezumi.extract('http://tv.sohu.com/20140318/n396785017.shtml', function(err, urls, options) {
+      console.log(err, urls, options);
+      done();
+    });
+  });
+  it('my sohu', function(done) {
+    nezumi.extract('http://my.tv.sohu.com/pl/8990047/80342721.shtml', function(err, urls, options) {
+      console.log(err, urls, options);
+      done();
+    });
+  });
 });
